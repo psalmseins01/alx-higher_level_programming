5bf997524c997149c9ffee01d45ff07fc86c0190
Anthony Gbobo <psalmseins@gmail.com>
2023-11-30 17:00:21 -0800

    Program that imports the variable a from the file

--- 0x02-python-import_modules/git.sh
+++ 0x02-python-import_modules/git.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-git add . && git commit -m "Print the result of the addition of all arguments" && git push
+git add . && git commit -m "Program that imports the variable a from the file" && git push
