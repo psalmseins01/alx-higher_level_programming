f54db0f88a95432272bfc7609647fc00498332cf
Anthony Gbobo <psalmseins@gmail.com>
2023-12-03 15:11:51 -0800

    Function that does exactly the same as python bytecode

--- 0x00-python-hello_world/git.sh
+++ 0x00-python-hello_world/git.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-git add . && git commit -m "Function that prints to standard error" && git push
+git add . && git commit -m "Function that does exactly the same as python bytecode" && git push
