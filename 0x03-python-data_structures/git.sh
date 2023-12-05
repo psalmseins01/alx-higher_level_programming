e233b048d384c61b15c70cae726b0bfddf8f6ec9
Anthony Gbobo <psalmseins@gmail.com>
2023-12-04 16:05:31 -0800

    Function that retrieves an element from a list

--- 0x03-python-data_structures/git.sh
+++ 0x03-python-data_structures/git.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-git add . && git commit -m "Function that prints all integers of a list" && git push
+git add . && git commit -m "Function that retrieves an element from a list" && git push
