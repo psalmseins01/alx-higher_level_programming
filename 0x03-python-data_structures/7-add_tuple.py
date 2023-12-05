25d31b9277ad65e5752dc047250d2c67aa68d026
Anthony Gbobo <psalmseins@gmail.com>
2023-12-04 18:52:35 -0800

    Function that adds 2 tuples.

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return (None)
-    else:
-        max_int = my_list[0]
-        for i in range(len(my_list)):
-            if my_list[i] > max_int:
-                max_int = my_list[i]
-        return (max_int)
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+
+    total_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return total_tuple
