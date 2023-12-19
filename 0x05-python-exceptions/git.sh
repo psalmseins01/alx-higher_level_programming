e6d9dc74fa88d46c91e2b21dcf2a800c696c5af0
Anthony Gbobo <psalmseins@gmail.com>
2023-12-18 18:28:34 -0800

    Write a function that executes a function safely.

--- 0x05-python-exceptions/git.sh
+++ 0x05-python-exceptions/git.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-git add . && git commit -m "Write a function that prints an integer" && git push 
+git add . && git commit -m "Write a function that executes a function safely." && git push 
