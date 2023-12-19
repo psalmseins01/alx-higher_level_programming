ffccb674d9077433a555e8f00d52f9b8b02e150c
Anthony Gbobo <psalmseins@gmail.com>
2023-12-18 17:10:03 -0800

    Write a function that divides 2 integers and prints the result

--- 0x05-python-exceptions/git.sh
+++ 0x05-python-exceptions/git.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-git add . && git commit -m "Write a function that prints the first x elements of a list and only integers." && git push 
+git add . && git commit -m "Write a function that divides 2 integers and prints the result" && git push 
