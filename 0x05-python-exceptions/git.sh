93d36b3cf34eedddb2684eb9b0086ddaba7ffbcb
Anthony Gbobo <psalmseins@gmail.com>
2023-12-18 18:20:04 -0800

    Write a function that prints an integer

--- 0x05-python-exceptions/git.sh
+++ 0x05-python-exceptions/git.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-git add . && git commit -m "Write a function that raises a name exception with a message." && git push 
+git add . && git commit -m "Write a function that prints an integer" && git push 
