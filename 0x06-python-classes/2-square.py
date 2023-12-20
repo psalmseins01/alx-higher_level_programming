ea1ec2111aa43b6992429268c58debba5c91d53e
Anthony Gbobo <psalmseins@gmail.com>
2023-12-19 16:39:07 -0800

    Class "Square" that defines a square by: (based on 1-square.py)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""class Square that defines a square by: (based on 1-square.py)"""
+
+
+class Square:
+    """Class that represents a suare"""
+
+    def __init__(self, size=0):
+        """Initializing this square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
--- 0x06-python-classes/git.sh
+++ 0x06-python-classes/git.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-git add . && git commit -m "Class \"Square\" that defines a square by: (based on 0-square.py)" && git push
+git add . && git commit -m "Class \"Square\" that defines a square by: (based on 1-square.py)" && git push
