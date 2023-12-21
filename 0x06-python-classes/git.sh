187889e6442a0298be63f33444a1ec1039d83075
Anthony Gbobo <psalmseins@gmail.com>
2023-12-20 17:28:00 -0800

    class Square that defines a square by: (based on 3-square.py)'

--- 0x06-python-classes/git.sh
+++ 0x06-python-classes/git.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-git add . && git commit -m "Translating a bytecode with a class 'MagicClass'" && git push
+git add . && git commit -m "class Square that defines a square by: (based on 3-square.py)'" && git push
