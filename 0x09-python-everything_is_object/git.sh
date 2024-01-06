b02018df36391cdb2e91e36c476342f2b5c4750a
Anthony Gbobo <psalmseins@gmail.com>
2024-01-05 19:12:30 -0800

    Function magic_string() that returns a string “BestSchool” n times the number of the iteration

--- 0x09-python-everything_is_object/git.sh
+++ 0x09-python-everything_is_object/git.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-git add . && git commit -m "if two list are thesame" && git push
+git add . && git commit -m "Function magic_string() that returns a string “BestSchool” n times the number of the iteration" && git push
