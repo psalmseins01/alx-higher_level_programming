625c85738a25d1a24fb4a673aa9362fbc946e7ca
Anthony Gbobo <psalmseins@gmail.com>
2024-01-06 20:01:23 -0800

    Write a class Rectangle that defines a rectangle by: (based on 5-rectangle.py)

--- 0x08-python-more_classes/git.sh
+++ 0x08-python-more_classes/git.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-git add . && git commit -m "Write a class Rectangle that defines a rectangle by: (based on 4-rectangle.py)" && git push
+git add . && git commit -m "Write a class Rectangle that defines a rectangle by: (based on 5-rectangle.py)" && git push
