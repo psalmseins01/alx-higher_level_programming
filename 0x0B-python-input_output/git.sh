1508a1be38cb91c41386ed281b5ddaca39f943d2
Anthony Gbobo <psalmseins@gmail.com>
2024-01-10 21:00:09 -0800

    Write a script that adds all arguments to a Python list, and then save them to a file:

--- 0x0B-python-input_output/git.sh
+++ 0x0B-python-input_output/git.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-git add . && git commit -m "Write a function that creates an Object from a “JSON file”:" && git push
+git add . && git commit -m "Write a script that adds all arguments to a Python list, and then save them to a file:" && git push
