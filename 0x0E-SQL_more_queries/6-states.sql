40bb36a61865bd012604de22408333179c52ac62
Anthony Gbobo <psalmseins@gmail.com>
2024-02-14 15:10:12 -0800

    Create a database called 'hbtn_0d_usa' and a table 'states'

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
--- Creates the table hbtn_0d_usa with table states.
+-- Creates a table 'hbtn_0d_usa' and a table called 'states'
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
     PRIMARY KEY(`id`),
-    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `id`   INT  NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(256) NOT NULL
 );
