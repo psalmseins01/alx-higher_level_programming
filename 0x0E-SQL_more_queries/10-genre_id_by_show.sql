5276611c5e93346db95a72d6238d0138c01a632f
Anthony Gbobo <psalmseins@gmail.com>
2024-02-14 18:11:47 -0800

    Script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,11 +1,10 @@
-/*
-  - Import the database dump from hbtn_0d_tvshows to your MySQL server: download
-  - Script to list all shows in hbtn_0d_tvshows with at least one linked genre.
-  - Each record displays: tv_shows.title - tv_show_genres.genre_id
-  - Results sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-  - One SELECT statement only.
-  - Database name passed as an argument in the mysql command.
-*/
+- Import the database dump from hbtn_0d_tvshows to your MySQL server: download
+- Script to list all shows in hbtn_0d_tvshows with at least one linked genre.
+- Each record displays: tv_shows.title - tv_show_genres.genre_id
+- Results sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+- One SELECT statement only.
+- Database name passed as an argument in the mysql command.
+
 SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
         INNER JOIN `tv_show_genres` AS g
