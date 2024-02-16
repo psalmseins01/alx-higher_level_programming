bc15734232509d3325a4aee287939158ae92f4ad
Anthony Gbobo <psalmseins@gmail.com>
2024-02-15 16:08:47 -0800

    Script that lists all rows of the table first_table from the database hbtn_0c_0

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,3 +1,4 @@
--- lists all rows of the table first_table
--- from the database hbtn_0c_0
+-- Lists all rows of 'first_table' from 'hbtn_0c_0' database in MySQL server
+-- All fields printed. Database name passed as argument in mysql command.
+
 SELECT * FROM first_table;
