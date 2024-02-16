d10e012026619cf815ea1093730ebec4ab27abd1
Anthony Gbobo <psalmseins@gmail.com>
2024-02-15 17:27:12 -0800

    Script that updates the score of Bob to 10 in the table second_table

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
--- updates teh score of Bob to 10 int the 
--- second table
--- using only the name field
+ -- Script to update Bob's score to 10 in 'second_table'.
+ -- Only Bob's name field will be used; his id value cannot be utilized.
+ -- Database name will be provided as an argument in the mysql command
 UPDATE second_table SET score = 10 WHERE name = 'Bob';
