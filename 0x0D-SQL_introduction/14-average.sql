9e3271602be66ec651cbcc6b8d10b5646eb7b3e8
Anthony Gbobo <psalmseins@gmail.com>
2024-02-15 19:13:28 -0800

    Computes average score of all records in 'second_table

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
--- computes score average of all records in the second_table
--- second table
+-- Computes the average score of all records in the 'second_table' of database
+-- 'hbtn_0c_0' in MySQL server. Result column named 'average'.
 SELECT AVG(score) AS average FROM second_table;
