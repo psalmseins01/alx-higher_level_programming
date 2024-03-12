7e835b46d26507eccfc04d4e120518e2d34863a4
Anthony Gbobo <psalmseins@gmail.com>
2024-03-11 17:47:21 -0700

    Script that prints 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 // Program that prints C is fun,Python is cool and JavaScript is amazing.
-const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(myVar);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
