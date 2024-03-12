30c0717dca54d8c6a2fbbf11e9b9e73ccfc55c3e
Anthony Gbobo <psalmseins@gmail.com>
2024-03-11 18:07:03 -0700

    Script that prints 3 lines

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// script to print strings using a loop
+
+let i;
+const printStr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+while (i < 3){
+	console.log(printStr[i]);
+	i++;
+}
