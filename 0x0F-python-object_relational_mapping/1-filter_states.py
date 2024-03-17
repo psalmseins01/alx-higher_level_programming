c488ecf67e8dacb1c87b8d33dc07b9a92bdecec1
Anthony Gbobo <psalmseins@gmail.com>
2024-03-17 15:46:37 -0700

    Get all states, Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""List out states"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn_db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn_db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    get_rows = cur.fetchall()
+    for row in get_rows:
+        if row[1].startswith("N"):
+            print(row)
+    cur.close()
+    conn_db.close()
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Lists out the states"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn_db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn_db.cursor()
+    query = """
+SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
+    query = query.format(argv[4])
+    cur.execute(query)
+    get_rows = cur.fetchall()
+    for row in get_rows:
+        print(row)
+    cur.close()
+    conn_db.close()
