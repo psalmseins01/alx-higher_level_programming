e0b70fa7d853036970d6c37ace2c506aeb2f57ac
Anthony Gbobo <psalmseins@gmail.com>
2024-03-17 16:25:58 -0700

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,8 +5,9 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    conn_db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+    conn_db = MySQLdb.connect(
+            host="localhost", port=3306, user=argv[1],
+            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn_db.cursor()
     query = """
 SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
