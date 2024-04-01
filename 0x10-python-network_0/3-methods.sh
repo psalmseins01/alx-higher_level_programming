dd66ff3e3f43c77b61f3e1127eb4344b71d3fdc7
Anthony Gbobo <psalmseins@gmail.com>
2024-03-31 17:14:55 -0700

    Bash script that takes in a URL and displays all HTTP methods the server will accept

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-#Bash script that takes in a URL and displays all HTTP methods
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-allowed_methods=$(curl -s -X OPTIONS -I "$1" | grep "Allow:" | tr -d '\r' | awk '{print $2}')
-if [ -z "$allowed_methods" ]; then
-  echo "No allowed methods found for $1"
-else
-  echo "Allowed HTTP methods for $1:"
-  echo "$allowed_methods"
-fi
+#Bash script that takes in a URL and displays all
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
