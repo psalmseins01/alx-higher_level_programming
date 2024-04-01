95a4a80aab41195c3991ca056c8168d590fcb2b0
Anthony Gbobo <psalmseins@gmail.com>
2024-03-31 18:47:26 -0700

    Correction

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,22 +10,22 @@ def find_peak(list_of_integers):
     Returns: peak of list_of_integers or None
     """
     size = len(list_of_integers)
-    mid_e = size
+    mid_elem = size
     mid = size // 2
 
     if size == 0:
         return None
 
     while True:
-        mid_e = mid_e // 2
+        mid_elem = mid_elem // 2
         if (mid < size - 1 and
                 list_of_integers[mid] < list_of_integers[mid + 1]):
-            if mid_e // 2 == 0:
-                mid_e = 2
-            mid = mid + mid_e // 2
-        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-            if mid_e // 2 == 0:
-                mid_e = 2
-            mid = mid - mid_e // 2
+            if mid_elem // 2 == 0:
+                mid_elem = 2
+            mid = mid + mid_elem // 2
+        elif mid_elem > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_elem // 2 == 0:
+                mid_elem = 2
+            mid = mid - mid_elem // 2
         else:
             return list_of_integers[mid]
